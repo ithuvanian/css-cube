9626201ec846410e6e523c4b34991b4406799699
Michael <michd.weaver@gmail.com>
2018-01-01 02:00:12 -0500

    added hover animations

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
 
-  function triggerAnimation(dot, spin) {
-    dot.on('mouseover', function() {
+  function tiltCube(div, tilt, reset) {
+
+    $(div).hover(function() {
+      $('.cube').removeClass('resetUp resetDown resetLeft resetRight resetUpLeft resetUpRight resetDownLeft resetDownRight').addClass(tilt);
+    }, function() {
+      setTimeout(function() {
+        $('.cube').removeClass('tiltUp tiltDown tiltLeft tiltRight tiltUpLeft tiltUpRight tiltDownLeft tiltDownRight').addClass(reset);
+      }, 100);
+      setTimeout(function() {
+        $('.cube').removeClass(reset);
+      }, 400);
+    });
+
+  }
+
+  function clickAnimation(dot, spin) {
+    dot.on('click', function() {
       $('.cube').addClass(spin);
       $('.direction').fadeOut();
       setTimeout(function() {
@@ -11,18 +26,29 @@ $(document).ready(function() {
     });
   }
 
-  triggerAnimation( $('.north'), 'spinUp');
-  triggerAnimation( $('.west'), 'spinLeft');
-  triggerAnimation( $('.east'), 'spinRight');
-  triggerAnimation( $('.south'), 'spinDown');
+  tiltCube($('.north'), 'tiltDown', 'resetDown');
+  tiltCube($('.south'), 'tiltUp', 'resetUp');
+  tiltCube($('.west'), 'tiltRight', 'resetRight');
+  tiltCube($('.east'), 'tiltLeft', 'resetLeft');
+  tiltCube($('.nw'), 'tiltDownRight', 'resetDownRight');
+  tiltCube($('.ne'), 'tiltDownLeft', 'resetDownLeft');
+  tiltCube($('.sw'), 'tiltUpRight', 'resetUpRight');
+  tiltCube($('.se'), 'tiltUpLeft', 'resetUpLeft');
+  clickAnimation($('.top'), 'spinDown');
+  clickAnimation($('.bottom'), 'spinUp');
+  clickAnimation($('.left'), 'spinRight');
+  clickAnimation($('.right'), 'spinLeft');
 
-  $('.cube').on('click', function() {
-    $(this).addClass('spinDiagonal');
+  $('.front').on('click', function() {
+    $('.cube').addClass('spinDiagonal');
     $('.direction').fadeOut();
     setTimeout(function() {
       $('.cube').removeClass('spinDiagonal');
       $('.direction').fadeIn();
-    }, 2000);
+    }, 1000);
   });
+
+
+
 
 });
